bcfcc3484b2956a22f170bfb3b6f55116ae7971f
abhishek <abhisingh562002offc@gmail.com>
2023-08-03 00:40:00 +0530

    final commit

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,7 @@
 import FeedbackItems from "./FeedbackItems";
 import PropTypes from "prop-types";
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet!!</p>;
   }
@@ -9,7 +9,7 @@ function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItems key={item.id} item={item}></FeedbackItems>
+        <FeedbackItems key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
